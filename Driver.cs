3cbb05ae226a8b6c9948f4c44d49d5d1461eafd8
mmendes1 <mmendes1@worcester.edu>
2021-10-19 07:52:17 -0400

    First commit

---
+++ Driver.cs
@@ -0,0 +1,36 @@
+﻿using System;
+//Next: User input
+namespace workout_app
+{
+    class Driver
+    {
+        static async void Main()
+        {
+            /**int userIn = 0;
+            while(userIn != 3) {
+                Console.WriteLine("Hello, please select a function...");
+                Console.WriteLine("   1. Print all workouts\n   2. Set a rep count.\n   3. Exit");
+                    userIn =Convert.ToInt32(Console.ReadLine());
+                        if(userIn == 1) {}
+                        else if (userIn == 2) {}
+                        else if (userIn == 3) Console.WriteLine("Enjoy the workout!");
+                        else Console.WriteLine("That is not a valid input, try again.");      **/
+             Workout pushups = new Workout ("Push-ups", "Back", 24);
+             Workout situps = new Workout ("Sit-ups", "Abs", 17);
+             Workout curls = new Workout ("Curls", "Biceps", 26);
+             Workout pullups = new Workout ("Pull-ups", "Shoulders", 19);
+             Workout diamonds = new Workout ("Diamonds", "Triceps", 15);
+             Workout squats = new Workout ("Squats", "Legs", 14);
+             Workout burndown = new Workout ("Shoulder-press", "Shoulders", 27);
+
+                //Console.WriteLine(curls.ToString());
+                 Workout.writeToFile(pushups);
+                Workout.writeToFile(situps);
+                Workout.writeToFile(curls);
+                Workout.writeToFile(pullups);
+                Workout.writeToFile(diamonds);
+                Workout.writeToFile(squats);
+               await Workout.writeToFile(burndown);
+            }
+        }
+    }
---
+++ Workout.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace workout_app
+{
+    public class Workout
+    {
+        public Workout() { }
+
+        public Workout(string name, string type, int repNum) {
+            workoutName = name;
+            workoutType = type;
+            workoutRepNum = repNum;
+        }
+
+        public static async Task writeToFile(Workout workout) {
+            using StreamWriter file = new("Workout_Data.txt", append: true);
+            await file.WriteLineAsync(workout.ToString());
+        }
+
+        public string workoutName {get; set; }
+        public string workoutType {get; set; }
+        public int workoutRepNum {get; set; }
+
+        public override string ToString() => workoutName + " " + workoutType + " " + workoutRepNum;
+    }
+}
