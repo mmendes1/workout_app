2a532a77ddfb12695136dfbcefe276473b795872
Mike Mendes <mmendes1@worcester.edu>
2021-10-19 13:17:46 -0400

    Populated data and set up main loop

--- Driver.cs
+++ Driver.cs
@@ -12,15 +12,17 @@ namespace workout_app
 
             Console.WriteLine("Hello, please select a function...");
             Console.WriteLine("   1. Print all workouts\n   2. Set a rep count.\n   3. Exit");
-
             do {
                     userIn = Console.ReadLine();
-                        if(userIn == "1") { temp.printWorkouts(); }
-                        else if (userIn == "2") { Console.WriteLine("To be implemented"); }
-                        else if (userIn == "3") Console.WriteLine("Enjoy the workout!");
-                        else Console.WriteLine("That is not a valid input, try again.");    
+                        if(userIn == "1") { temp.printWorkouts(); Console.WriteLine(); }
+                        else if (userIn == "2") { Console.WriteLine("To be implemented\n"); }
+                        else if (userIn == "3") Console.WriteLine("Enjoy the workout!\n");
+                        else Console.WriteLine("That is not a valid input, try again.\n");    
             } while(userIn != "3");
-             /**
+        }
+    }
+}
+ /**
              Workout pushups = new Workout ("Push-ups", "Back", 24);
              Workout situps = new Workout ("Sit-ups", "Abs", 17);
              Workout curls = new Workout ("Curls", "Biceps", 26);
@@ -38,8 +40,3 @@ namespace workout_app
                 recordData(squats);
                 recordData(burndown);
                 **/
-        }
-
-        
-    }
-}
